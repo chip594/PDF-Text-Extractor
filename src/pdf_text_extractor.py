717161bf6b101f808d0e7a94a9af2ea4ff32f9cd
Chip Newman <chip594@gmail.com>
2019-01-18 13:47:41 -0600

    trying to open all files

--- src/pdf_text_extractor.py
+++ src/pdf_text_extractor.py
@@ -1,12 +1,13 @@
 #   pdf_text_extractor.py
 
 import sys
+import os
 from tika import parser
 from tika import unpack
 import numpy
 
 
-input_file = sys.argv[1]
+#input_file = sys.argv[1]
 
 badges = []
 names = []
@@ -16,9 +17,11 @@ name_title = {}
 parsed_file = open('parsed_file.txt', 'w')
 output_file = open('output.txt', 'w')
 
-parsed = unpack.from_file(input_file)
-
-parsed_file.write(parsed['content'])
+file_list = os.listdir(os.getcwd())
+for files in file_list:
+    if files.endswith('.pdf'):
+        parsed = unpack.from_file(files)
+        parsed_file.write(parsed['content'])
 parsed_file.close()
 
 with open('parsed_file.txt', 'r') as f:
