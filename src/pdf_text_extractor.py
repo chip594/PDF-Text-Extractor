bd0a53dbc41e8150aee25863a784ea8f9efea229
Chip Newman <chip594@gmail.com>
2019-01-18 06:39:02 -0600

    added code to try tika

--- src/pdf_text_extractor.py
+++ src/pdf_text_extractor.py
@@ -1,1 +1,14 @@
 #   pdf_text_extractor.py
+
+from tika import parser
+
+names = []
+
+raw = parser.from_file('input0.pdf')
+output_file = open('output.txt', 'w')
+
+name = ''.join(raw['content'])
+
+print(name)
+
+#   print(raw['content'])
