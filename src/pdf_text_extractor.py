f1fc189eb9c6f606543b4667f655690e096c3dce
Chip Newman <chip594@gmail.com>
2019-01-19 21:03:41 -0600

    changed output file based on user input

--- src/pdf_text_extractor.py
+++ src/pdf_text_extractor.py
@@ -2,8 +2,20 @@
 
 import os
 from tika import unpack
+import pymsgbox
 
-output_file = open('output.txt', 'w')
+def add_txt_ext(user_input):
+    if len(user_input) < 1:
+        return '_output'
+    else:
+        return user_input
+
+user_input = pymsgbox.prompt('Enter the desiered name of the output file: (without ".txt")', default=add_txt_ext(''), title='FBPI .pdf Text Extractor')
+
+if user_input == None:
+    exit(0)
+
+output_file = open(add_txt_ext(user_input) + '.txt', 'w')
 
 file_list = os.listdir(os.getcwd())
 for files in file_list:
