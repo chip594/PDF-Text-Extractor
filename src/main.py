a793dfa11bcca6b8d8e0b465256911636110f47b
Chip Newman <chip594@gmail.com>
2019-02-05 16:59:28 -0600

    added function to get user input

--- src/main.py
+++ src/main.py
@@ -6,3 +6,10 @@ import pymsgbox
 from extractor import PDFExtractor
 from formatter import TextFormatter
 
+# returs a name of the output file
+def get_user_input():
+    user_input = pymsgbox.prompt('Enter name', default=add_txt_ext(''), title='FBPI .pdf Text Extractor')
+    # closes program if user clicks cancel
+    if user_input == None:
+        exit(0)
+    return user_input
