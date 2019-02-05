72f5f9e53749150faec4550c0241d4fb64faa1d0
Chip Newman <chip594@gmail.com>
2019-02-04 21:53:48 -0600

    creates list to store the badge info

--- src/formatter.py
+++ src/formatter.py
@@ -5,3 +5,5 @@ class TextFormatter(object):
     def name_tab_title(self, text):
         # stores contents of the input text into a list
         name_badge = text.split('\n')
+
+        badges = []
