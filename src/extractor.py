6739d945fe5fb13c56636988f355798d55587bdd
Chip Newman <chip594@gmail.com>
2019-02-04 21:38:11 -0600

    added function to extract text from a pdf

--- src/extractor.py
+++ src/extractor.py
@@ -1,2 +1,12 @@
 # extractor.py
 
+from io import StringIO
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+
+class PDFExtractor(object):
+
+    def pdf_to_text(self, pdf_file, pages=None):
+        pass
